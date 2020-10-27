bac39d2d2497b7ae924651222295fcfa5235e927
richardnoel1337 <richardnoel1337@gmail.com>
2020-10-27 18:57:47 -0400

    added the ability to create comments, currently uses some dummy data

--- angular/src/app/modules/lodging/lodging-details/lodging-details.component.ts
+++ angular/src/app/modules/lodging/lodging-details/lodging-details.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Lodging } from '../../../data/lodging.model';
 import { ActivatedRoute } from '@angular/router';
 import { LodgingService } from '../../../services/lodging/lodging.service';
+import { Review } from 'data/review.model';
+import * as moment from 'moment';
+import { Profile } from 'data/profile.model';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'uic-lodging-details',
@@ -12,6 +16,9 @@ export class LodgingDetailsComponent implements OnInit {
    * fields used in this component
    */
   lodging: Lodging | null = null;
+  review: Review;
+  profile: Profile;
+  Comment: FormGroup;
 
   /**
    * provide activated route to get route parameters and lodging service to get lodging
@@ -21,7 +28,34 @@ export class LodgingDetailsComponent implements OnInit {
   constructor(
     private readonly route: ActivatedRoute,
     private readonly lodgingService: LodgingService
-  ) {}
+  ) {
+
+
+    this.Comment = new FormGroup({
+      score: new FormControl(''),
+      message: new FormControl(''),
+    })
+
+    //Populating the profile with dummy data
+    this.profile = {
+      id: "1",
+      email: "Email@email.com",
+      type: "adult",
+      givenName: "Guy",
+      familyName: "Ferri",
+      phone: "111-111-1111"
+    }
+    
+    //Populating the review with empty values
+    this.review = {
+      id: "",
+      name: "",
+      comment: "",
+      dateCreated: "",
+      rating: 0
+    };
+
+  }
 
   /**
    * On init, get the lodging for which the details will be displayed
@@ -39,7 +73,8 @@ export class LodgingDetailsComponent implements OnInit {
       if (idString) {
         this.lodgingService.getById(idString).subscribe((data) => {
           this.lodging = data;
-
+          this.lodging.reviews = [];
+          console.log(this.lodging)
           this.lodgingService.getImages(idString).subscribe((urls) => {
             if (this.lodging != null) {
               this.lodging.imageUrls = urls;
@@ -48,5 +83,25 @@ export class LodgingDetailsComponent implements OnInit {
         });
       }
     });
+  }
+
+  /*
+  * Posts the user's comment on the lodging details page
+  */
+  OnSubmit(){
+    this.review.id = this.profile.id;
+    this.review.comment = this.Comment.get('message')?.value;
+    this.review.dateCreated = moment().format('MMMM Do YYYY, h:mm:ss a');
+    this.review.rating = this.Comment.get('score')?.value;
+
+    //Grabs the name from local storage
+    let tempName = JSON.parse(localStorage.getItem("okta-token-storage") as string);
+    this.review.name = tempName.idToken.claims.name;
+
+    //Adds the review to lodging reviews array
+    this.lodging?.reviews.push(this.review);
+
+    //Implementing adding the review to backend at a later date
+    // this.lodgingService.put();
   }
 }
