55badbb37c7b9d5f490cbc60d5fb967ad0339d44
AndrewMarron <amarron314@gmail.com>
2020-10-27 17:49:14 -0400

    Completed issue 66 (featured booking hover and click behavior)

--- angular/src/app/modules/booking/featured-lodging/featured-lodging.component.ts
+++ angular/src/app/modules/booking/featured-lodging/featured-lodging.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { Lodging } from 'src/app/data/lodging.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'uic-featured-lodging',
@@ -9,9 +10,17 @@ export class FeaturedLodgingComponent implements OnChanges {
   @Input() featuredLodgings!: Lodging[] | null;
   displayLodgings: Lodging[] = [];
 
+  constructor(private router: Router) { }
+
   ngOnChanges(): void {
     if (this.featuredLodgings) {
       this.displayLodgings = this.featuredLodgings.slice(0, 6);
     }
+  }
+
+  featureClick(thisLodging: Lodging){
+    console.log("Going to lodging details for"+ thisLodging.name);
+    // console.log(thisLodging);
+    this.router.navigate(['/lodging/details/'+ thisLodging.id]);
   }
 }
