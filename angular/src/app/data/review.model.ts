b6ac83b8e2f370da4a451539d52cc83d46024923
richardnoel1337 <richardnoel1337@gmail.com>
2020-10-28 12:59:55 -0400

    changed the ids in the models to be numbers instead of strings to work with the backend

--- angular/src/app/data/review.model.ts
+++ angular/src/app/data/review.model.ts
@@ -9,13 +9,15 @@
  * ```
  */
 export interface Review {
-  id: string;
+  accountId: number;
   /**profile name */
-  name: string;
+  name?: string;
   /** text body */
   comment: string;
   /** date the review was posted */
   dateCreated: string;
   /** integer rating out of ten stars */
   rating: number;
+  /**lodging id to see what lodging the review belongs to */
+  lodgingId: number;
 }
