ebde2aa78584d0d8f6ad972098216469de1d4cbc
AndrewMarron <amarron314@gmail.com>
2020-10-28 15:18:38 -0400

    Issue 66: Updated tooltip to list lot availability by type

--- angular/src/app/modules/booking/featured-lodging/featured-lodging.component.ts
+++ angular/src/app/modules/booking/featured-lodging/featured-lodging.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { Lodging } from 'src/app/data/lodging.model';
 import { Router } from '@angular/router';
+import { Rental } from '../../../data/rental.model';
 
 @Component({
   selector: 'uic-featured-lodging',
@@ -9,18 +10,66 @@ import { Router } from '@angular/router';
 export class FeaturedLodgingComponent implements OnChanges {
   @Input() featuredLodgings!: Lodging[] | null;
   displayLodgings: Lodging[] = [];
+  lotAvailableStringsByLodgingId = new Map<string, string[]>();
 
   constructor(private router: Router) { }
 
   ngOnChanges(): void {
     if (this.featuredLodgings) {
       this.displayLodgings = this.featuredLodgings.slice(0, 6);
+      this.setAvailableCountsByType();
     }
+  }
+
+  /**
+   * Sets a map property in this.displayLodgings to keep track of availible rentals by type 
+   */
+  public setAvailableCountsByType(){
+    console.log("Lodgings:")
+    console.log(this.displayLodgings)
+    for(const thisLodging of this.displayLodgings){
+      const thisLodgingId = thisLodging.id;
+      let lodgingLotTypes: string[] = [];
+      let totalCountByType = new Map<string, number>();
+      let availableCountByType = new Map<string, number>();
+      for(const thisLot of thisLodging.rentals){
+        const thisLotType = thisLot.unit.name;
+        if(lodgingLotTypes.indexOf(thisLotType) == -1){
+          lodgingLotTypes.push(thisLotType)
+        }
+        let incrementIfAvailable = 0;
+        if(thisLot.status.toLocaleLowerCase() === 'available'){
+          incrementIfAvailable = 1;
+        }
+        let thisTypeCount = availableCountByType.get(thisLotType);
+        if (thisTypeCount == undefined) {
+          availableCountByType.set(thisLotType, incrementIfAvailable);
+        }else{
+          availableCountByType.set(thisLotType, thisTypeCount + incrementIfAvailable);
+        }
+        let thisTypeTotalCount = totalCountByType.get(thisLotType);
+        if (thisTypeTotalCount == undefined) {
+          totalCountByType.set(thisLotType, 1);
+        }else{
+          totalCountByType.set(thisLotType, thisTypeTotalCount + 1);
+        }
+      }
+      let thisLodgingLotCountStrings: string[] = [];
+      for(let tInc = 0; tInc < lodgingLotTypes.length; tInc ++){
+        const thisLodgingLotType = lodgingLotTypes[tInc];
+        let thisLodgingTypeCount = availableCountByType.get(thisLodgingLotType);
+        let thisLodgingTypeTotalCount = totalCountByType.get(thisLodgingLotType);
+        thisLodgingLotCountStrings.push(thisLodgingLotType + ": "
+          + thisLodgingTypeCount +" of "+ thisLodgingTypeTotalCount + " Available");
+      }
+      this.lotAvailableStringsByLodgingId.set(thisLodgingId, thisLodgingLotCountStrings)
+    }
+    console.log("Lodging count strings:")
+    console.log(this.lotAvailableStringsByLodgingId)
   }
 
   featureClick(thisLodging: Lodging){
     console.log("Going to lodging details for"+ thisLodging.name);
-    // console.log(thisLodging);
     this.router.navigate(['/lodging/details/'+ thisLodging.id]);
   }
 }
