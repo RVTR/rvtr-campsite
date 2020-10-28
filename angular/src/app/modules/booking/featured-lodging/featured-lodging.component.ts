fb8bf5d92bca25842af5b65151f53de97d301653
Jon Sledge <jonsledge39@gmail.com>
2020-10-28 15:40:24 -0400

    fixing minor type errors and styling

--- angular/src/app/modules/booking/featured-lodging/featured-lodging.component.ts
+++ angular/src/app/modules/booking/featured-lodging/featured-lodging.component.ts
@@ -10,9 +10,9 @@ import { Rental } from '../../../data/rental.model';
 export class FeaturedLodgingComponent implements OnChanges {
   @Input() featuredLodgings!: Lodging[] | null;
   displayLodgings: Lodging[] = [];
-  lotAvailableStringsByLodgingId = new Map<string, string[]>();
+  lotAvailableStringsByLodgingId = new Map<number, string[]>();
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnChanges(): void {
     if (this.featuredLodgings) {
@@ -22,54 +22,60 @@ export class FeaturedLodgingComponent implements OnChanges {
   }
 
   /**
-   * Sets a map property in this.displayLodgings to keep track of availible rentals by type 
+   * Sets a map property in this.displayLodgings to keep track of availible rentals by type
    */
-  public setAvailableCountsByType(){
-    console.log("Lodgings:")
-    console.log(this.displayLodgings)
-    for(const thisLodging of this.displayLodgings){
+  public setAvailableCountsByType() {
+    console.log('Lodgings:');
+    console.log(this.displayLodgings);
+    for (const thisLodging of this.displayLodgings) {
       const thisLodgingId = thisLodging.id;
       let lodgingLotTypes: string[] = [];
       let totalCountByType = new Map<string, number>();
       let availableCountByType = new Map<string, number>();
-      for(const thisLot of thisLodging.rentals){
+      for (const thisLot of thisLodging.rentals) {
         const thisLotType = thisLot.unit.name;
-        if(lodgingLotTypes.indexOf(thisLotType) == -1){
-          lodgingLotTypes.push(thisLotType)
+        if (lodgingLotTypes.indexOf(thisLotType) == -1) {
+          lodgingLotTypes.push(thisLotType);
         }
         let incrementIfAvailable = 0;
-        if(thisLot.status.toLocaleLowerCase() === 'available'){
+        if (thisLot.status.toLocaleLowerCase() === 'available') {
           incrementIfAvailable = 1;
         }
         let thisTypeCount = availableCountByType.get(thisLotType);
         if (thisTypeCount == undefined) {
           availableCountByType.set(thisLotType, incrementIfAvailable);
-        }else{
+        } else {
           availableCountByType.set(thisLotType, thisTypeCount + incrementIfAvailable);
         }
         let thisTypeTotalCount = totalCountByType.get(thisLotType);
         if (thisTypeTotalCount == undefined) {
           totalCountByType.set(thisLotType, 1);
-        }else{
+        } else {
           totalCountByType.set(thisLotType, thisTypeTotalCount + 1);
         }
       }
       let thisLodgingLotCountStrings: string[] = [];
-      for(let tInc = 0; tInc < lodgingLotTypes.length; tInc ++){
+      for (let tInc = 0; tInc < lodgingLotTypes.length; tInc++) {
         const thisLodgingLotType = lodgingLotTypes[tInc];
         let thisLodgingTypeCount = availableCountByType.get(thisLodgingLotType);
         let thisLodgingTypeTotalCount = totalCountByType.get(thisLodgingLotType);
-        thisLodgingLotCountStrings.push(thisLodgingLotType + ": "
-          + thisLodgingTypeCount +" of "+ thisLodgingTypeTotalCount + " Available");
+        thisLodgingLotCountStrings.push(
+          thisLodgingLotType +
+            ': ' +
+            thisLodgingTypeCount +
+            ' of ' +
+            thisLodgingTypeTotalCount +
+            ' Available'
+        );
       }
-      this.lotAvailableStringsByLodgingId.set(thisLodgingId, thisLodgingLotCountStrings)
+      this.lotAvailableStringsByLodgingId.set(thisLodgingId, thisLodgingLotCountStrings);
     }
-    console.log("Lodging count strings:")
-    console.log(this.lotAvailableStringsByLodgingId)
+    console.log('Lodging count strings:');
+    console.log(this.lotAvailableStringsByLodgingId);
   }
 
-  featureClick(thisLodging: Lodging){
-    console.log("Going to lodging details for"+ thisLodging.name);
-    this.router.navigate(['/lodging/details/'+ thisLodging.id]);
+  featureClick(thisLodging: Lodging) {
+    console.log('Going to lodging details for' + thisLodging.name);
+    this.router.navigate(['/lodging/details/' + thisLodging.id]);
   }
 }
