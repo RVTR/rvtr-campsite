8d16520bd5154c74b05a75d6a300230b31fda65f
Jupshaw97 <70708707+Jupshaw97@users.noreply.github.com>
2020-10-29 15:44:24 -0400

    updated version, booking supports images

--- angular/src/app/modules/booking/featured-lodging/featured-lodging.component.ts
+++ angular/src/app/modules/booking/featured-lodging/featured-lodging.component.ts
@@ -9,9 +9,21 @@ export class FeaturedLodgingComponent implements OnChanges {
   @Input() featuredLodgings!: Lodging[] | null;
   displayLodgings: Lodging[] = [];
 
+  imageUrls: string[] = [];
+
   ngOnChanges(): void {
     if (this.featuredLodgings) {
       this.displayLodgings = this.featuredLodgings.slice(0, 6);
+      console.log('Lodging before adding imageUrl: ', this.displayLodgings);
+
+      // Initialize the array of url strings to be an empty array
+      for (const lodging of this.displayLodgings) {
+        // DUMMY DATA, change later once the lodgings have their own image urls already
+        lodging.imageUrls = [];
+        lodging.imageUrls[0] =
+          'https://btrtoday.sfo2.digitaloceanspaces.com/uploads/Featured-1.png';
+      }
+      console.log('Lodging after adding imageUrl: ', this.displayLodgings);
     }
   }
 }
--- angular/src/app/modules/booking/spotlight/spotlight.component.ts
+++ angular/src/app/modules/booking/spotlight/spotlight.component.ts
@@ -10,17 +10,43 @@ export class SpotlightComponent implements OnChanges {
   selectedLodging: Lodging | null = null;
   spotlight = false;
 
+  // Need to change this to an array of strings later because the lodgings hold an array of url strings!
+  imageUrl!: string;
+
   ngOnChanges(): void {
     this.setSpotlight(this.lodgings);
   }
 
   setSpotlight(lodgings: Lodging[] | null): void {
     let temp = 0;
+    this.imageUrl = '';
+    // Initializing the array of url strings to be empty
+    if (lodgings) {
+      for (const lodging of lodgings) {
+        lodging.imageUrls = [];
+      }
+    }
+    console.log('Lodgings: ', this.lodgings);
     if (lodgings) {
       for (const lodging of lodgings) {
         if (lodging.rentals.length > temp) {
           temp = lodging.rentals.length;
           this.selectedLodging = lodging;
+          console.log('Lodging:', lodging);
+          console.log('SelectedLodging:', this.selectedLodging);
+          // Change later!!
+          if (this.selectedLodging) {
+            // DUMMY DATA! Make imageURL an array and have a for loop that selects all the urls from selectedLodging
+            this.selectedLodging.imageUrls[0] =
+              'https://play-lh.googleusercontent.com/niSCOgqAwJyjNwyRbu1QvsBw0CM1DQg8ypE3_cbMWSnsI-0saVU3HDfPsbtYlT9ie4Q=w200-h300-rw';
+            this.imageUrl = this.selectedLodging.imageUrls[0];
+          }
+          // Dynamically allocate the url strings from the selected lodging to the imageUrl array for display
+          // this.selectedLodging.imageUrls.forEach(url => {
+          //   console.log("Pushing url to imageURL array: ", url);
+          //   this.imageUrl.push(url);
+          // });
+          // console.log("Url array after for each: ", this.imageUrl);
         }
       }
     }
